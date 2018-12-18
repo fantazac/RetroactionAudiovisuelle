f01de29c253e8a6bb9b74e87af9645f94a1bd542
didier1 gerschheimer <didier1.gerschheimer@epitech.eu>
2018-12-18 13:22:32 -0500

    Utility: Player grid position and action range

---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utility
+{
+    public static Vector2Int GetPlayerActionPosition(Transform player)
+    {
+        float playerRange = .5f; // Distance the player 'arms' can reach from player position
+        
+        Vector3 actionPosition = player.position + player.rotation * Vector3.forward * playerRange;
+        
+        return GetGridPosition(actionPosition);
+    }
+    
+    public static Vector2Int GetGridPosition(Vector3 worldPosition)
+    {
+        return new Vector2Int((int) worldPosition.x, (int) worldPosition.z);
+    }
+}
